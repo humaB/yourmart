41bd4da786675601a759d2709ea9371a247e6cfb
Adnan Asad <adnanasad@outlook.com>
2024-09-03 15:11:47 +0500

    Not Found Route for API added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,5 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'JwtToken' => \App\Http\Middleware\JwtToken::class
     ];
 }
