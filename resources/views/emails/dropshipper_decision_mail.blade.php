c9447b09857f0f74e2a532988f830d7ad36104d4
Adnan Asad <adnanasad@outlook.com>
2024-09-24 02:29:44 +0500

    Inventory purchase order setup added

--- resources/views/emails/dropshipper_decision_mail.blade.php
+++ resources/views/emails/dropshipper_decision_mail.blade.php
@@ -56,21 +56,17 @@
         @if ($mailData['decision'] == 'reject')
         <p>Dear {{ $mailData['full_name'] }},</p>
         <p>Thank you for applying to become a dropshipper with YourMart. After carefully reviewing your application, we regret to inform you that it has not been approved at this time.</p>
-        <ul>
-            <li><strong>We encourage you to review our requirements and consider reapplying in the future. If you have any questions or need clarification, please feel free to contact us at 0326 9810000.</strong></li>
-            <li><strong>We appreciate your interest in partnering with us and wish you success in your future endeavors.</strong></li>
-            <li><strong>Best regards,</strong></li>
-            <li><strong>YourMart Team</strong></li>
-        </ul>
+        <p>We encourage you to review our requirements and consider reapplying in the future. If you have any questions or need clarification, please feel free to contact us at 0326 9810000.</p>
+        <p>We appreciate your interest in partnering with us and wish you success in your future endeavors.</p>
+        <p>Best regards,</p>
+        <p>YourMart Team</p>
         @else
             <p>Dear {{ $mailData['full_name'] }},</p>
             <p>Congratulations! Your dropshipping application with YourMart has been Approved.</p>
-            <ul>
-                <li><strong>Please Log in to start managing your orders. For assistance, contact us at 0326 9810000.</strong></li>
-                <li><strong>Welcome aboard!</strong></li>
-                <li><strong>Best regards,</strong></li>
-                <li><strong>YourMart Team</strong></li>
-            </ul>
+            <p>Please Log in to start managing your orders. For assistance, contact us at 0326 9810000.</p>
+            <p>Welcome aboard!</p>
+            <p>Best regards,</p>
+            <p>YourMart Team</p>
         @endif
         </div>
 
