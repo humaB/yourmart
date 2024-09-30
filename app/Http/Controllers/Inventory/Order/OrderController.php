08d71e42861325553fbacd4bd519ae6628e545c4
Adnan Asad <adnanasad@outlook.com>
2024-10-01 01:17:08 +0500

    Our charges added

--- app/Http/Controllers/Inventory/Order/OrderController.php
+++ app/Http/Controllers/Inventory/Order/OrderController.php
@@ -77,7 +77,7 @@ class OrderController extends Controller
 
     public function updateStatus( Request $request ){
 
-        return $userRole  = trim(auth()->user()->role);
+        $userRole  = trim(auth()->user()->role);
 
         $order = Order::with('items')->find($request->id);
 
