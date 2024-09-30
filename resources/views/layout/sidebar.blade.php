191fd73cec6b43afff5ed01003116ef15acf8362
Adnan Asad <adnanasad@outlook.com>
2024-09-30 20:03:57 +0500

    Our charges added

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -31,7 +31,7 @@
         @include('layout.other_sidebar.inventory_sidebar')
     @endif
 
-    @if( auth()->user()->role == 'qc manager')
+    @if( auth()->user()->role == 'qc manger')
         @include('layout.other_sidebar.dailer_sidebar')
     @endif
 
