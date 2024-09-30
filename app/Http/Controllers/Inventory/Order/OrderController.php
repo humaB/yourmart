0b668077d48de9126646d31802c9ce09317f3a8b
Adnan Asad <adnanasad@outlook.com>
2024-10-01 01:52:50 +0500

    Our charges added

--- app/Http/Controllers/Inventory/Order/OrderController.php
+++ app/Http/Controllers/Inventory/Order/OrderController.php
@@ -210,7 +210,8 @@ class OrderController extends Controller
                     'added_by'   => auth()->user()->id
                 ]);
 
-                ProductVariation::where('id', $product->product_variation_id)->increment('stock', $product->quantity);
+                ProductVariation::where('id', $product->product_variation_id)
+                ->increment('stock', $product->quantity);
             }
         }else{
             $order->update(['status' => '6']);
