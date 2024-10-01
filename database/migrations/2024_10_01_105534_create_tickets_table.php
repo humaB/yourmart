825143012cd9d2709539f071627916112aa1fafc
abdul rehman <malik.rehman7272@gmail.com>
2024-10-01 17:04:57 +0500

    change

---
+++ database/migrations/2024_10_01_105534_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('ticket_type');
+            $table->text('message'); 
+            $table->text('expected_result')->nullable(); 
+            $table->string('file_path')->nullable(); 
+            $table->string('status')->default('In-Process'); 
+            $table->bigInteger('added_by')->nullable();
+            $table->bigInteger('updated_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
