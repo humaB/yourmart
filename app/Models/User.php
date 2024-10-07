1f728e3427629becd09a2f4675a5a953c1e161cb
Adnan Asad <adnanasad@outlook.com>
2024-10-07 12:30:32 +0500

    jwt setup removed

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,26 +46,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
-
     public function dropshipper(){
         return $this->hasOne(DropShipper::class, 'user_id', 'id');
     }
