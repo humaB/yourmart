f07131df126ccfa654c304c35e4a8e404e896eae
Adnan Asad <adnanasad@outlook.com>
2024-10-24 16:13:48 +0500

    Admin dashboard widgets added

--- app/Http/Controllers/User/DropShipperController.php
+++ app/Http/Controllers/User/DropShipperController.php
@@ -817,7 +817,7 @@ public function fetchDetails(Request $request)
         $pdf->Cell(180, 0, '* Errors and omissions excepted (E&OE)', 0, 1, 'C', 0, '', 0, false, 'T', 'M');
         $pdf->Ln(3);
         $pdf->SetFont('times', 'B', 9);
-        $pdf->Cell(180, 0, 'Printed By : ' . $user_name . ' || ' . $date_now, 0, 0, 'C', 0, '', 0, false, 'T', 'M');
+        $pdf->Cell(180, 0, 'Printed By : ' . $user_name . ' || ' . $date_now, 0, 1, 'C', 0, '', 0, false, 'T', 'M');
         $pdf->SetFont('times', '', 8);
         $pdf->Cell(180, 0, 'Developed by SAR ZONE', 0, 1, 'C', 0, '', 0, false, 'T', 'M');
 
