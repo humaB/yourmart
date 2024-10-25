35fc04f83e19360ae3609871dbc942699d40b9ba
Adnan Asad <adnanasad@outlook.com>
2024-10-25 17:23:53 +0500

    avg price updated

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -17,24 +17,6 @@ class DashboardController extends Controller
 
          $orders = Order::where('status', '8')->pluck('id');
 
-         $productsWithMaxPrice = DB::table('inventory_purchase_order_store_received_details')
-         ->select('product_id', DB::raw('MAX(price) as max_price'))
-         ->groupBy('product_id')
-         ->get();
-
-
-            foreach($productsWithMaxPrice as $setPrice) {
-                $product = Product::with('variation')->find($setPrice->product_id);
-
-                if ($product && filled($product->variation)) {
-                    ProductVariation::where('id', $product->variation->id)->update([
-                        'avg_price' => $setPrice->max_price
-                    ]);
-                }
-            }
-
-
-
          $top10SellingProducts = OrderItem::with('variation.product')
             ->whereIn('order_id', $orders)
             ->select('product_variation_id', DB::raw('SUM(price * quantity) as selling_price'), DB::raw('SUM(quantity) as total_quantity'))
