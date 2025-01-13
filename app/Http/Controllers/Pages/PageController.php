91d4a833090fde1f84f3e2997bf144477d895834
Adnan Asad <adnanasad@outlook.com>
2025-01-13 17:28:33 +0500

    home page test

--- app/Http/Controllers/Pages/PageController.php
+++ app/Http/Controllers/Pages/PageController.php
@@ -28,7 +28,7 @@ class PageController extends Controller
                 if (isset($image['link']) && isset($image['button_link']) && !empty($image['button_link'])) {
                    $imageIndex =  'image-' . ($image['index'] + 1);
                     // Check if an image record exists, update or create new
-                   $homePageSetting = HomePageSetting::where('type', $imageIndex)->first();
+                   return $homePageSetting = HomePageSetting::where('type', $imageIndex)->first();
 
                     if ($homePageSetting) {
                         return $homePageSetting;
