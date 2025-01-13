7981098e3d7d97746f975111c80486fa99b41866
Adnan Asad <adnanasad@outlook.com>
2025-01-13 17:14:48 +0500

    home page test

--- app/Http/Controllers/Pages/PageController.php
+++ app/Http/Controllers/Pages/PageController.php
@@ -39,6 +39,7 @@ class PageController extends Controller
 
                         // Only update 'attachment' if 'link' is provided
                         if (!empty($image['link']) && $image['link'] != 'undefined') {
+                            return "sad";
                             $updateData['attachment'] = $this->homeBanner($image['link']); // Attachment as image
                         }
 
