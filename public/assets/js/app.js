ae7b1371630897084b4e71cd0ba9b949d5679b52
Adnan Asad <adnanasad@outlook.com>
2025-02-01 20:45:07 +0500

    cash image displayed

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -12516,15 +12516,15 @@ var render = function render() {
     staticClass: "img-thumbnail img-responsive",
     attrs: {
       alt: "attachment",
-      src: "".concat(_vm.web_url, "public/storage/uploads/payments/").concat(_vm.details.payment_proof_attachment)
+      src: "".concat(_vm.web_url).concat(_vm.details.type === "Cash" ? "public/uploads/dropshipper/payments/" : "public/storage/uploads/payments/").concat(_vm.details.payment_proof_attachment)
     }
   })]), _vm._v(" "), _c("a", {
     staticClass: "name",
     attrs: {
-      href: "".concat(_vm.web_url, "public/storage/uploads/payments/").concat(_vm.details.payment_proof_attachment),
+      href: "".concat(_vm.web_url).concat(_vm.details.type === "Cash" ? "public/uploads/dropshipper/payments/" : "public/storage/uploads/payments/").concat(_vm.details.payment_proof_attachment),
       target: "_blank"
     }
-  }, [_vm._v("\n                                                    " + _vm._s(_vm.truncatedAttachmentName(_vm.details.payment_proof_attachment)) + "\n                                                ")])]) : _vm._e(), _vm._v(" "), _vm._l(_vm.details.attachments, function (item) {
+  }, [_vm._v("\n                                                  " + _vm._s(_vm.truncatedAttachmentName(_vm.details.payment_proof_attachment)) + "\n                                                ")])]) : _vm._e(), _vm._v(" "), _vm._l(_vm.details.attachments, function (item) {
     return _vm.details.attachments.length > 0 ? _c("div", {
       key: item.id,
       staticClass: "col-md-2"
--- resources/js/components/inventory/product/order/OrderDetailView.vue
+++ resources/js/components/inventory/product/order/OrderDetailView.vue
@@ -457,17 +457,16 @@
                                             </p>
                                             <div class="row" v-if="details">
                                                 <div class="col-md-2" v-if="details.payment_proof_attachment">
-                                                    <a target="_blank"
-                                                        :href="setImage(details.payment_proof_attachment)">
-                                                        <img class="img-thumbnail img-responsive" alt="attachment"
-                                                            :src="`${web_url}public/storage/uploads/payments/${details.payment_proof_attachment}`">
+                                                    <a target="_blank" :href="setImage(details.payment_proof_attachment)">
+                                                      <img class="img-thumbnail img-responsive" alt="attachment"
+                                                           :src="`${web_url}${details.type === 'Cash' ? 'public/uploads/dropshipper/payments/' : 'public/storage/uploads/payments/'}${details.payment_proof_attachment}`">
                                                     </a>
                                                     <a class="name"
-                                                        :href="`${web_url}public/storage/uploads/payments/${details.payment_proof_attachment}`"
-                                                        target="_blank">
-                                                        {{ truncatedAttachmentName(details.payment_proof_attachment) }}
+                                                       :href="`${web_url}${details.type === 'Cash' ? 'public/uploads/dropshipper/payments/' : 'public/storage/uploads/payments/'}${details.payment_proof_attachment}`"
+                                                       target="_blank">
+                                                      {{ truncatedAttachmentName(details.payment_proof_attachment) }}
                                                     </a>
-                                                </div>
+                                                  </div>
 
                                                    <!-- Attachments from comments array -->
                                                    <div class="col-md-2" v-for="item in details.attachments"  :key="item.id" v-if="details.attachments.length > 0">
