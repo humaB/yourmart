a8bfffcabe92d81e90d0962f69c0fdfd5f3c3a87
Adnan Asad <adnanasad@outlook.com>
2025-05-30 01:57:34 +0500

    timezone changed and log added

--- app/Http/Controllers/Helpers/PostExApiHelper.php
+++ app/Http/Controllers/Helpers/PostExApiHelper.php
@@ -248,7 +248,7 @@ class PostExApiHelper
 
         if (isset($this->shipmentStatuses[$order['orderStatus']]) && $detail && $detail->status != 8 && $detail->status != 9) {
             $status = $this->shipmentStatuses[$order['orderStatus']];
-
+            Log::info($request);
             //If product is delivered
             if ($status['label'] == 'Delivered' && $detail->status != '8') {
                 $helper = new LeopardApiHelper();
