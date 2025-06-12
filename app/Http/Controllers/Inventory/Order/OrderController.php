7a2ec58630bfb3a71c13971d8c89508f914a88ac
Adnan Asad <adnanasad@outlook.com>
2025-06-12 20:09:26 +0500

    shipper code changed

--- app/Http/Controllers/Inventory/Order/OrderController.php
+++ app/Http/Controllers/Inventory/Order/OrderController.php
@@ -646,7 +646,7 @@ class OrderController extends Controller
 
                 if ($order->courier_service_id == '2') {
                     $postExApi = new PostExApiHelper();
-                    return $postExData = $postExApi->bookAPacket($order, $order->order_no, $order->shop_id);
+                    $postExData = $postExApi->bookAPacket($order, $order->order_no, $order->shop_id);
 
                     if ($postExData['error'] && $postExData['error'] != '') {
                         return (new ValidationCollection([$postExData['error']]))
@@ -710,7 +710,7 @@ class OrderController extends Controller
 
                 if ($order->courier_service_id == '2') {
                     $postExApi = new PostExApiHelper();
-                    return $postExData = $postExApi->bookAPacket($order, $order->order_no, $order->shop_id);
+                    $postExData = $postExApi->bookAPacket($order, $order->order_no, $order->shop_id);
 
                     if ($postExData['error'] && $postExData['error'] != '') {
                         return (new ValidationCollection([$postExData['error']]))
