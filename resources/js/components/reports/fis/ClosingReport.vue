45a410e902562c570c7a682413b3a1a1743eff61
Adnan Asad <adnanasad@outlook.com>
2025-06-27 16:53:22 +0500

    Daily Business Report date range added

--- resources/js/components/reports/fis/ClosingReport.vue
+++ resources/js/components/reports/fis/ClosingReport.vue
@@ -52,7 +52,7 @@
                                         <td>{{ item.leopards }}</td>
                                         <td>{{ item.daraz }}</td>
                                         <td>{{ item.cash }}</td>
-                                        <td>{{ item.totalOrders }}</td>
+                                        <td>{{ item.totalOrders }}</td> 
                                         <td>{{ formatPrice(item.totalSales) }}</td>
                                         <td>{{ item.postExReturns }}</td>
                                         <td>{{ item.leopardReturns }}</td>
