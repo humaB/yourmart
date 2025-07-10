505e09ad070c9abbdfdfea05467ccd78655ed4b5
Adnan Asad <adnanasad@outlook.com>
2025-07-10 15:42:50 +0500

    log file added

--- app/Http/Controllers/Helpers/PostExApiHelper.php
+++ app/Http/Controllers/Helpers/PostExApiHelper.php
@@ -277,6 +277,7 @@ class PostExApiHelper
             }
         }
 
+        Log::info($request);
         if (isset($this->shipmentStatuses[$order['orderStatus']]) && $detail && $detail->status != 8 && $detail->status != 9) {
 
             $status = $this->shipmentStatuses[$order['orderStatus']];
