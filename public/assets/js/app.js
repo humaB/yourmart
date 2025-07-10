5ef96d1a9c7a9e8c42089f2e71d0261ea1df50f9
Adnan Asad <adnanasad@outlook.com>
2025-07-10 15:44:09 +0500

    log file added

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -20652,6 +20652,10 @@ var render = function render() {
     }
   }, [_vm._v("Awaiting Your Reply")]), _vm._v(" "), _c("option", {
     attrs: {
+      value: "Awaiting YourMart Reply"
+    }
+  }, [_vm._v("Awaiting YourMart Reply")]), _vm._v(" "), _c("option", {
+    attrs: {
       value: "Closed"
     }
   }, [_vm._v("Closed")]), _vm._v(" "), _c("option", {
