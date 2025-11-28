23318c5c63d2aaa2a3e826a2bd6efb8e31454cfc
humaB <huma.mahmood02@gmail.com>
2025-11-28 21:52:58 +0500

    data correction

--- app/Http/Controllers/GrowthController.php
+++ app/Http/Controllers/GrowthController.php
@@ -89,6 +89,7 @@ $orderbelongsto = $orders->pluck('belongs_to')->unique()->values();
         foreach ($issues as $singleProductGroup) {
             $quantity = $singleProductGroup->sum('quantity');
     
+            // Calculate Avg Purchase Price
             $rate = StoreReceivedDetail::where('created_at', '<=', $date)
                 ->where('product_id', $singleProductGroup[0]->product_id)
                 ->select(DB::raw("SUM(total) / SUM(quantity) as rate"))
@@ -122,7 +123,7 @@ $orderbelongsto = $orders->pluck('belongs_to')->unique()->values();
             $totalProfit += $profit;
         }
     
-        return round($totalProfit, 2);
+        return (int) round($totalProfit);
     }
     
 }
